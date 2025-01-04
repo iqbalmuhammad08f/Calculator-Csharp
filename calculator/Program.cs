c495ea3566c9720a0e5151af202d8888748fbcae
iqbalmuhammad08f <iqbalnurhakimy83@gmail.com>
2025-01-04 13:29:27 +0700

    revisi

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -1,11 +1,10 @@
 ﻿using System;
-using System.Linq;
 
 class Calculator
 {
-    static int Menghitung(int angka1,string operasi,int angka2)
+    static double Menghitung(double angka1,string operasi,double angka2)
     {
-        switch(operasi)
+        switch (operasi)
         {
             case "+":
                 return angka1 + angka2;
@@ -25,17 +24,35 @@ class Calculator
         Console.WriteLine();
         while (true)
         {
-            Console.Write("Pilih operasi(+,-,*,/): ");
-            var operasi = Console.ReadLine();
+            Console.Write("Pilih operasi(+,-,*,/,0=keluar): ");
+            var operasi = Console.ReadLine() ?? string.Empty;
             string[] validOperator = { "+", "-", "*", "/" };
             if (validOperator.Contains(operasi))
             {
                 Console.Write("Masukkan angka pertama: ");
-                int angka1 = Convert.ToInt32(Console.ReadLine());
+                double angka1 = Convert.ToInt32(Console.ReadLine());
                 Console.Write("Masukkan angka kedua: ");
-                int angka2 = Convert.ToInt32(Console.ReadLine());
-                int hasil = Menghitung(angka1, operasi, angka2);
-                Console.WriteLine($"Hasilnya adalah {hasil}");
+                double angka2 = Convert.ToInt32(Console.ReadLine());
+                if (operasi == "/" && angka2 == 0)
+                {
+                    Console.WriteLine("tidak bisa membagi dengan 0, enter untuk mengulangi");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+                else
+                {
+                    double hasil = Menghitung(angka1, operasi, angka2);
+                    Console.WriteLine($"Hasilnya adalah {hasil}");
+                    Console.WriteLine("enter untuk megulangi");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+                
+            }
+            else if (operasi == "0")
+            {
+                Console.Clear();
+                break;
             }
             else
             {
